8429662fd6749e853f29a126a6821ae9f3336aef
m9795 <106650955+m9795@users.noreply.github.com>
2022-08-03 15:17:20 +0900

    [Add]routes設定,host許可

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   # ユーザー用
-  devise_for :customers, skip: [:passwords], controllers: {
+  devise_for :user, skip: [:passwords], controllers: {
     registrations: 'public/registrations',
     sessions: 'public/sessions'
   }
-  
+
   # 管理者用
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
