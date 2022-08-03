4387ffa3936cbb81bf6e397c87905ee2f1f1b559
m9795 <106650955+m9795@users.noreply.github.com>
2022-08-04 01:42:34 +0900

    [Update]投稿詳細,一覧の画像サイズ変更など

--- app/models/post.rb
+++ app/models/post.rb
@@ -23,9 +23,13 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :taggings, dependent: :destroy
 
-  has_many_attached :image
+  has_one_attached :image
 
   def get_image
-    (image.attached?) ? image: "no_image.jpg"
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    image
   end
 end
