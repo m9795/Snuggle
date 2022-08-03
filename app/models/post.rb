7772220f68739ecd263cbd834703f2a34a99ac81
m9795 <106650955+m9795@users.noreply.github.com>
2022-08-03 15:55:03 +0900

    [Add]アソシエーション

--- app/models/post.rb
+++ app/models/post.rb
@@ -19,4 +19,7 @@
 #
 class Post < ApplicationRecord
   belongs_to :user
+  has_many :likes,    dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :taggings, dependent: :destroy
 end
