449fbfbff1d6c0b584fbbcba07118ad46241d3ef
m9795 <106650955+m9795@users.noreply.github.com>
2022-08-03 20:09:26 +0900

    [Update]user,adminモデル不要カラムをコメントアウト

--- db/migrate/20220803044857_devise_create_users.rb
+++ db/migrate/20220803044857_devise_create_users.rb
@@ -4,18 +4,15 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string  :name,               null: false
-      t.text    :introduction
-      t.boolean :status,             null: false, default: "false"
       t.string  :email,              null: false, default: ""
       t.string  :encrypted_password, null: false, default: ""
 
       ## Recoverable
-      t.string   :reset_password_token
-      t.datetime :reset_password_sent_at
+      # t.string   :reset_password_token
+      # t.datetime :reset_password_sent_at
 
       ## Rememberable
-      t.datetime :remember_created_at
+      # t.datetime :remember_created_at
 
       ## Trackable
       # t.integer  :sign_in_count, default: 0, null: false
@@ -34,7 +31,9 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
       # t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
-
+      t.string  :name,          null: false
+      t.text    :introduction
+      t.boolean :status,        null: false, default: "false"
 
       t.timestamps null: false
     end
