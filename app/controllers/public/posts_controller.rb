aacc6d09ad9bac13e0f551261dc431c03a43ea6e
m9795 <106650955+m9795@users.noreply.github.com>
2022-08-03 18:24:00 +0900

    [Update]ルーティング

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -2,10 +2,22 @@ class Public::PostsController < ApplicationController
   def new
   end
 
+  def create
+  end
+
   def index
   end
 
+  def show
+  end
+  
   def edit
+  end
+
+  def update
+  end
+
+  def destroy
   end
 
   def search
