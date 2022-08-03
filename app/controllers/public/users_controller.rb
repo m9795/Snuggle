a6b26a5b19fa286f80d12fec5a8c3ede70e00a60
m9795 <106650955+m9795@users.noreply.github.com>
2022-08-04 03:27:03 +0900

    [Add]users/edit機能の追加

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -9,14 +9,26 @@ class Public::UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    user = User.find(params[:id])
+    if user.update(usre_params)
+      redirect_to user_path(user), notice: "ユーザ情報を更新しました。"
+    else
+      render 'edit'
+    end
   end
 
   def unsubscribe
   end
 
   def search
+  end
+
+  private
+  def usre_params
+    params.require(:user).permit(:name, :introduction, :status)
   end
 end
