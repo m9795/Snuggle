bcf231e870be662feb44d1f23866423373a72dba
m9795 <106650955+m9795@users.noreply.github.com>
2022-08-03 22:27:29 +0900

    [Update]ヘッダー部分テンプレート化

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -4,7 +4,16 @@ class Public::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
   before_action :configure_permitted_parameters, if: :devise_controller?
+  
+  def after_sign_up_path_for(resource)
+    user_path(current_user)
+  end
 
+  protected
+  
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
   # GET /resource/sign_up
   # def new
   #   super
@@ -39,15 +48,7 @@ class Public::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  def after_sign_up_path_for(resource)
-    user_path(current_user)
-  end
 
-  protected
-  
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_up_params
