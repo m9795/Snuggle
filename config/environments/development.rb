080320314f4f7fb5307ed0d698e479622d69264c
m9795 <106650955+m9795@users.noreply.github.com>
2022-08-04 23:35:01 +0900

    [Add]better_errors

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,10 +52,10 @@ Rails.application.configure do
 
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
-  
+
   #gem 'image_processing'のエラー回避
   config.active_job.queue_adapter = :inline
-  
+
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
@@ -77,4 +77,7 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
   config.hosts << "c337219d2b8d4cc4a0837b58bf2202bd.vfs.cloud9.ap-northeast-1.amazonaws.com"
+
+  # gem 'better_errors'のエラー回避
+  BetterErrors::Middleware.allow_ip! "0.0.0.0/0"
 end
