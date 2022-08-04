c817f6d96197fc55022abb569102e351097cff82
m9795 <106650955+m9795@users.noreply.github.com>
2022-08-04 22:21:20 +0900

    [Update]コメント機能　非同期化,エラー文出たままになる

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -6,22 +6,26 @@ class Public::CommentsController < ApplicationController
   end
 
   def create
-    post = Post.find(params[:post_id])
-    comment = current_user.comments.new(comment_params)
-    comment.post_id = post.id
-    comment.save
-    redirect_to new_post_comment_path(post)
+    @post = Post.find(params[:post_id])
+    @comment = current_user.comments.new(comment_params)
+    @comment.post_id = @post.id
+    if @comment.save
+      # flash.now[:notice] = 'コメントを投稿しました。'
+      render 'post_comments'
+    else
+      render 'error'
+    end
   end
 
   def destroy
+    @post = Post.find(params[:post_id])
     Comment.find(params[:id]).destroy
-    redirect_to request.referer
+    # flash.now[:alert] = 'コメントを削除しました。'
+    render 'post_comments'
   end
-
 
   private
-
-  def comment_params
-    params.require(:comment).permit(:comment)
-  end
+    def comment_params
+      params.require(:comment).permit(:comment)
+    end
 end
