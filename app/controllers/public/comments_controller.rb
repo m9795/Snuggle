fa93c6c6d7faf6f32ef0d7c35357c916ebd04895
m9795 <106650955+m9795@users.noreply.github.com>
2022-08-04 17:56:44 +0900

    [Add]コメント投稿機能作成中

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -1,13 +1,13 @@
 class Public::CommentsController < ApplicationController
 
   def new
-    @post = Post.find(params[:id])
+    @post = Post.find(params[:post_id])
     @comment = Comment.new
   end
 
   def create
     post = Post.find(params[:post_id])
-    comment = current_user.comments.new(comment_params)
+    comment = current_user.comment.new(comment_params)
     comment.post_id = post.id
     comment.save
     redirect_to post_path(post)
@@ -15,10 +15,10 @@ class Public::CommentsController < ApplicationController
 
   def destroy
   end
-  
-  
+
+
   private
-  
+
   def comment_params
     params.require(:comment).permit(:text)
   end
