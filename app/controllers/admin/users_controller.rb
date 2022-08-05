e7aff4e37ef35161e5814b8e169861e6a1a08d19
m9795 <106650955+m9795@users.noreply.github.com>
2022-08-05 17:24:26 +0900

    [Add]管理者　ユーザ管理機能作成中

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,13 +1,22 @@
 class Admin::UsersController < ApplicationController
   def index
+    @users = User.all
   end
 
   def show
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    user = User.find(params[:id])
+    user.update(user_params)
+    redirect_to users_path
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :status)
   end
 end
