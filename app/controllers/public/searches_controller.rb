fa55543e0e9a03fd166190d4a0c1e9220f091b4f
m9795 <106650955+m9795@users.noreply.github.com>
2022-08-06 22:46:40 +0900

    [Add]キーワード検索

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -1,9 +1,6 @@
 class Public::SearchesController < ApplicationController
   def search
-    if (params[:keyword])[0] == '#'
-      @post = Tag.search(params[:keyword]).order('created_at DESC')
-    else
-      @post = Post.search(params[:keyword]).order('created_at DESC')
-    end
+    @keyword = params[:keyword]
+    @post = Post.search(@keyword)
   end
 end
