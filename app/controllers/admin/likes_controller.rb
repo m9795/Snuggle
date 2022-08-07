acb8b17856a0049abb3403f2b0676bea2bbaa792
m9795 <106650955+m9795@users.noreply.github.com>
2022-08-07 19:44:13 +0900

    [Add]いいね削除機能の追加

---
+++ app/controllers/admin/likes_controller.rb
@@ -0,0 +1,11 @@
+class Admin::LikesController < ApplicationController
+  def index
+    @user = User.find(params[:user_id])
+    @likes = @user.likes
+  end
+
+  def destroy
+    Like.find(params[:id]).destroy
+    redirect_to admin_user_likes_path, notice: 'いいねを削除しました。'
+  end
+end
---
+++ test/controllers/admin/likes_controller_test.rb
@@ -0,0 +1,8 @@
+require "test_helper"
+
+class Admin::LikesControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get admin_likes_index_url
+    assert_response :success
+  end
+end
