1ced8341bf5e71ea909cfe927de86768267c8083
m9795 <106650955+m9795@users.noreply.github.com>
2022-08-08 20:32:05 +0900

    [Update]いいねした投稿一覧の件数修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
-  before_action :all_quantity
+  before_action :publish_quantity
 
   # ヘッダーの件数（退会ユーザー、非公開記事を省く処理）
-  def all_quantity
+  def publish_quantity
     @publish_user_all = User.where(status: false)
     @publish_post_all = Post.where(user_id: @publish_user_all, publish: true)
   end
