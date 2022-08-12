d8891a6fe70b5c7b9aff04d39800a7bdbc198604
m9795 <106650955+m9795@users.noreply.github.com>
2022-08-12 17:45:05 +0900

    [Update]フッターレイアウト

--- app/models/user.rb
+++ app/models/user.rb
@@ -36,7 +36,7 @@ class User < ApplicationRecord
   has_one_attached :image
   def get_image
     unless image.attached?
-      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      file_path = Rails.root.join('app/assets/images/neko16.jpg')
       image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
     image
