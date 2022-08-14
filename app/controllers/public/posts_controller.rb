b7f9e8be67b1d6b4da195bf062403682ac9aa7f0
m9795 <106650955+m9795@users.noreply.github.com>
2022-08-14 16:29:17 +0900

    [Add]ユーザ一覧ページにページネーション追加

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -17,6 +17,7 @@ class Public::PostsController < ApplicationController
 
 # 退会ユーザーと非公開記事を省いて取得
   def index
+    @posts = @publish_post_all.page(params[:page]).per(5)
     @current_user_posts = current_user.posts.where(id: @publish_post_all)
     @liked_post = current_user.likes.where(post_id: @publish_post_all)
   end
