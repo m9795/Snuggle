2014f167634e25707a269d535ff91f6f40fe307b
m9795 <106650955+m9795@users.noreply.github.com>
2022-08-27 15:00:00 +0900

    [Update]ログイン後のルートパスの遷移変更

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -2,6 +2,11 @@
 
 class Public::HomesController < ApplicationController
   def top
+    if user_signed_in?
+      redirect_to user_path(current_user)
+    elsif admin_signed_in?
+      redirect_to admin_users_path
+    end
   end
 
   def about
