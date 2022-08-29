988c1ef9e8ae8663718cfdced2977e1e9f614646
m9795 <106650955+m9795@users.noreply.github.com>
2022-08-29 22:17:58 +0900

    [Update]管理者側のコメント管理機能修正

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -9,9 +9,7 @@ class Admin::CommentsController < ApplicationController
   end
 
   def destroy
-    @user = User.find(params[:user_id])
-    @comments = @user.comments.order(created_at: "DESC")
-    @page_comments = @comments.page(params[:page]).per(20)
+    @comments = Post.find(params[:post_id]).comments.order(created_at: "DESC")
     Comment.find(params[:id]).destroy
     render "admin/comments/comments"
   end
