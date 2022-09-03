9c7c3a35436c292cf8175f23782967a5569a0235
EC2 Default User <ec2-user@ip-172-31-23-50.ap-northeast-1.compute.internal>
2022-09-03 03:49:06 +0000

    [Update]

--- app/controllers/public/free_posts_controller.rb
+++ app/controllers/public/free_posts_controller.rb
@@ -5,12 +5,24 @@ class Public::FreePostsController < ApplicationController
   end
 
   def create
+    @free_post = FreePost.new(free_post_params)
+    @free_post.user_id = current_user.id
+    if @free_post.save
+      redirect_to free_posts_path, notice: "投稿しました。"
+    else
+      redirect_to request.referer, alert: "入力内容をご確認ください。"
+    end
   end
 
   def index
+    @free_posts = FreePost.all
   end
 
   def destroy
   end
 
+  private
+    def free_post_params
+      params.require(:free_post).permit(:content)
+    end
 end
