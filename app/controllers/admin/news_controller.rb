a8002cd701a794d8b91df4dd1892b5416e3938eb
m9795 <m.ogawa9795@gmail.com>
2022-10-02 07:51:42 +0000

    [Add]おしらせ機能

--- app/controllers/admin/news_controller.rb
+++ app/controllers/admin/news_controller.rb
@@ -1,4 +1,6 @@
 class Admin::NewsController < ApplicationController
+  before_action :authenticate_admin!
+  before_action :news_choise, only: [:edit, :update, :destroy]
   def index
     @news = News.new
     @newses = News.post_pagenation(params[:page])
@@ -13,8 +15,22 @@ class Admin::NewsController < ApplicationController
   def edit
   end
 
+  def update
+    @news.update(news_params)
+    redirect_to admin_news_index_path, notice: "お知らせの内容を更新しました。"
+  end
+
+  def destroy
+    @news.destroy
+    redirect_to request.referer, notice: "削除しました。"
+  end
+
   private
     def news_params
       params.require(:news).permit(:title, :content)
+    end
+
+    def news_choise
+      @news = News.find(params[:id])
     end
 end
